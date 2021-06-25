3fc75cadf48615746a5b28f34ebaeb3f7638259d
shoko-inori <frank.gong.1006@hotmail.com>
2021-06-25 16:05:28 -0400

    Implement the image widget on the login page

---
+++ lib/login_page/login_picture.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Image widget on the login page
+class LoginPicture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage('assets/images/PEloginImage.png'),
+      fit: BoxFit.contain,
+      height: MediaQuery.of(context).size.height * 0.4,
+      width: MediaQuery.of(context).size.width * 0.8,
+    );
+  }
+}
